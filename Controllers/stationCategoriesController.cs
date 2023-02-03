428fd419a4e82b498a6cd49ac824621602ff85c5
Syed Hassan Ali <hassanalisyed799@gmail.com>
2023-02-03 17:42:49 +0500

    updated on 3/2/2027

--- Controllers/stationCategoriesController.cs
+++ Controllers/stationCategoriesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,57 +10,99 @@ using puma.Models;
 
 namespace puma.Controllers
 {
+    public class Data
+    {
+        public int catid { get; set; }
+        public int stid { get; set; }
+        public int stcatid { get; set; }
+        public List<int> stcatidlist { get; set; }
+        public string ?stname { get; set; }
+        public string ?catname { get; set; }
+    
+    }
+
     public class Category
     {
         public int ID { get; set; }
-        public string Name { get; set; }
+        public List<int> ids { get; set; }
+        public string ?Name { get; set; }
     }
-    public class stationdetails 
+    public class Stationdetails 
     { 
         public int ID { get; set; }
-        public string Name { get; set; }
+        public string ?Name { get; set; }
     }
 
-    public class lists
+    public class Lists
     {
-        public List<stationdetails> stationnames { get; set; }
-        public List<Category> categories { get; set; }
+        public List<Stationdetails> ?stations { get; set; }
+        public List<Category> ?categories { get; set; }
 
+    }
+    public class Editing
+    {
+        public int stcatid { get; set; }
+        public int stationid { get; set; }
+        public int categoryid { get; set; }
+        public string catname { get; set; }
+        public string stationname { get; set; }
+
+    }
+    public class detailsdata {
+    public List<string> detailscat { get; set; }
     }
 
     public class stationCategoriesController : Controller
     {
         private readonly dbContext _context;
-
-        public stationCategoriesController(dbContext context)
+ 
+    public stationCategoriesController(dbContext context)
         {
             _context = context;
         }
-      
+
         // GET: stationCategories
         public async Task<IActionResult> Index()
         {
+            var testdata = (from table1 in _context.StationMasters
+                            join table2 in _context.stationCategories on table1.StationId equals table2.StationId
+                            join table3 in _context.categoryMaster on table2.categoryId equals table3.categoryId
+                            select new Data
+                            {
+                                catid = table3.categoryId,
+                                stid = table1.StationId,
+                                catname = table3.categoryName,
+                                stname = table1.Name,
+                                stcatid = table2.stationCategoryId
+                            }).ToListAsync();
+
+
+
             return _context.stationCategories != null ?
-                        View(await _context.stationCategories.ToListAsync()) :
+                        View(await testdata) :
                         Problem("Entity set 'dbContext.stationCategories'  is null.");
         }
-
+   
         // GET: stationCategories/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.stationCategories == null)
-            {
-                return NotFound();
-            }
+            var query = _context.StationMasters.Where(x => x.StationId == id).ToList();
+            var mappedcat = (from a in _context.stationCategories where a.StationId==id select a.categoryId).ToList();
+           ViewBag.categoryNames = (from b in _context.categoryMaster where mappedcat.Contains(b.categoryId) select b.categoryName).ToList();
 
-            var stationCategory = await _context.stationCategories
-                .FirstOrDefaultAsync(m => m.stationCategoryId == id);
-            if (stationCategory == null)
-            {
-                return NotFound();
-            }
+            //if (id == null || _context.stationCategories == null)
+            //{
+            //    return NotFound();
+            //}
 
-            return View(stationCategory);
+            //var stationCategory = await _context.stationCategories
+            //    .FirstOrDefaultAsync(m => m.stationCategoryId == id);
+            //if (stationCategory == null)
+            //{
+            //    return NotFound();
+            //}
+
+            return View(query);
         }
 
         // GET: stationCategories/Create
@@ -70,16 +113,12 @@ namespace puma.Controllers
           List<int> stationids=list.Select(m=>m.StationId).ToList();
             var stationList = (from s in _context.StationMasters
                               where stationids.Contains(s.StationId)
-                              select new stationdetails {
+                              select new Stationdetails {
                                   ID=s.StationId,
                                   Name=s.Name
                               }).ToList();
 
 
-            //    stationnames
-            //.Where(s => stationids.Contains(s.StationId))
-            //.Select(s => s.Name)
-            //.ToList();
 
             var clist = _context.categoryMaster.ToList();
             List<int> catid= clist.Select(m=>m.categoryId).ToList();
@@ -90,17 +129,15 @@ namespace puma.Controllers
                                ID = c.categoryId,
                                Name = c.categoryName
                            }).ToList();
-            // .Where(s => catid.Contains(s.categoryId))
-            // .Select(s => s.categoryName)
-            //  .ToList();
-            lists obj = new lists() {
+           
+            Lists obj = new Lists() {
 
                 categories = catlist,
-                stationnames= stationList
+                stations= stationList
 
             };
 
-            SelectList stationlist = new SelectList(obj.stationnames.ToList());
+            SelectList stationlist = new SelectList(obj.stations.ToList());
             List<Category> categorylist = obj.categories.ToList();
             ViewBag.stationlist = stationlist;
             ViewBag.categories = categorylist;  
@@ -114,22 +151,19 @@ namespace puma.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("StationId, categoryId")] stationCategory stationCategory, IEnumerable<int> catids)
         {
-            //var query = from a in _context.stationCategories where a.stationCategoryId == _context select a;
-          
-          
+                    
             if (ModelState.IsValid)
             {
                 foreach (var item in catids)
                 {
                     var newStationCategory = new stationCategory
                     {
+                       
                         StationId = stationCategory.StationId,
                         categoryId = item
                     };
                     _context.Add(newStationCategory);
-                    //stationCategory.categoryId = item;
-                    //_context.Add(stationCategory);
-                  
+          
                 }
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -137,20 +171,51 @@ namespace puma.Controllers
             return View(stationCategory);
         }
 
-        // GET: stationCategories/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.stationCategories == null)
-            {
-                return NotFound();
-            }
 
-            var stationCategory = await _context.stationCategories.FindAsync(id);
-            if (stationCategory == null)
-            {
-                return NotFound();
-            }
-            return View(stationCategory);
+
+        // GET: stationCategories/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var idd = (from a in _context.stationCategories where a.StationId == id select a).ToList();
+           var list = _context.StationMasters.ToList();
+          List<int> stationids=list.Select(m=>m.StationId).ToList();
+            var stationList = (from s in _context.StationMasters
+                              where stationids.Contains(s.StationId)
+                              select new Stationdetails {
+                                  ID=id,
+                                  Name=s.Name
+                              }).ToList();
+
+
+
+            var clist = _context.categoryMaster.ToList();
+            List<int> catid= clist.Select(m=>m.categoryId).ToList();
+            var catlist = (from c in _context.categoryMaster
+                           where catid.Contains(c.categoryId)
+                           select new Category
+                           {
+                              ID=c.categoryId,
+                               Name = c.categoryName
+                           }).ToList();
+          
+            // .Where(s => catid.Contains(s.categoryId))
+            // .Select(s => s.categoryName)
+            //  .ToList();
+            Lists obj = new Lists() {
+
+                categories = catlist,
+                stations= stationList
+
+            };
+
+            SelectList stationlist = new SelectList(obj.stations.ToList());
+            List<Category> categorylist = obj.categories.ToList();
+            ViewBag.stationlist = stationlist;
+            ViewBag.categories = categorylist;
+
+            ViewBag.stationid = id;
+
+            return View(obj);
         }
 
         // POST: stationCategories/Edit/5
@@ -158,47 +223,49 @@ namespace puma.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("stationCategoryId,StationId,categoryId")] stationCategory stationCategory)
+        public async Task<IActionResult> Edit(IEnumerable<int> catids, [Bind("stationCategoryId,StationId,categoryId")] stationCategory stationCategory)
         {
-          
-            if (id != stationCategory.stationCategoryId)
+            var recordsToDelete = _context.stationCategories.Where(x => x.StationId == stationCategory.StationId);
+            if (recordsToDelete.Any())
             {
-                return NotFound();
+                _context.stationCategories.RemoveRange(recordsToDelete);
+                _context.SaveChanges();
             }
-
+            var stid=(from a in _context.stationCategories where a.StationId == stationCategory.StationId select a.stationCategoryId).ToList();
+        
             if (ModelState.IsValid)
             {
-                try
+                foreach (var item in catids)
                 {
-                    _context.Update(stationCategory);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!stationCategoryExists(stationCategory.stationCategoryId))
+                    var newStationCategory = new stationCategory
                     {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                        stationCategoryId = stid.FirstOrDefault(),
+                        StationId = stationCategory.StationId,
+                        categoryId = item                       
+                    };
+                    _context.Update(newStationCategory);
+                  
+
                 }
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(stationCategory);
+            return View("Index");
         }
+
+
 
         // GET: stationCategories/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
+
             if (id == null || _context.stationCategories == null)
             {
                 return NotFound();
             }
 
             var stationCategory = await _context.stationCategories
-                .FirstOrDefaultAsync(m => m.stationCategoryId == id);
+                .FirstOrDefaultAsync(m => m.StationId == id);
             if (stationCategory == null)
             {
                 return NotFound();
@@ -216,13 +283,15 @@ namespace puma.Controllers
             {
                 return Problem("Entity set 'dbContext.stationCategories'  is null.");
             }
-            var stationCategory = await _context.stationCategories.FindAsync(id);
-            if (stationCategory != null)
+            //var abc = _context.stationCategories.GroupBy(id => _context.stationCategories.Select(x => x.StationId));
+            var recordsToDelete = _context.stationCategories.Where(x => x.StationId == id);
+            if (recordsToDelete.Any())
             {
-                _context.stationCategories.Remove(stationCategory);
+                _context.stationCategories.RemoveRange(recordsToDelete);
+                await _context.SaveChangesAsync();
             }
 
-            await _context.SaveChangesAsync();
+     
             return RedirectToAction(nameof(Index));
         }
 
--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<dbContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options => {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+        options.EnableSensitiveDataLogging();
+        });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
